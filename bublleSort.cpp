81e869d918f5efea16b382372f981b80bd503ce6
FreireCF <clecioferreira011@gmail.com>
2025-04-11 19:28:45 -0300

    organizando arquivos

--- bublleSort.cpp
+++ bublleSort.cpp
@@ -1,19 +1,23 @@
 #include <stdio.h> 
-#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void){
     int n;
     printf("\nInsira o tamanho do vetor: ");
         scanf("%d",&n);
-    int vetor[n];
+    int* vetor = (int*) malloc(n * sizeof(int));
+    int ordenado, troca;
 
     printf("\nPreenchendo o vetor com %d números aleatórios",n);
     printf("\n...");
 
-    for(int i=0; i<n; i++){
-        vetor[i] = rand()%10;
-    }
+    srand(time(NULL));
 
+    for(int i=0; i<n; i++){
+        vetor[i] = rand()%100;
+    }
+    printf("\n");
     printf("\nVetor preenchido: ");
     printf("[");
     for(int i=0; i<n; i++){
@@ -21,14 +25,30 @@ int main(void){
     }
     printf("]");
 
+    printf("\n");
     printf("\nOrdenando o vetor");
     printf("\n...");
 
-    for(int i=0; i<n; i++){
-        if(vetor[i] > vetor [i+1]){
-            
-        }
+    do {
+        troca=0;
+        for(int i=0; i<n-1; i++){
+            if(vetor[i] > vetor [i+1]){
+                ordenado = vetor[i];
+                vetor[i] = vetor[i+1];
+                vetor[i+1] = ordenado;
+                troca = 1;    
+            } 
     }
+} while (troca==1);
 
+    printf("\n");
+    printf("\nVetor ordenado: ");
+    printf("[");
+    
+    for(int i=0; i<n; i++){
+        printf(" %d ",vetor[i]);
+    }
+    printf("]");
+    free(vetor);
     printf("\n \n");
 }
