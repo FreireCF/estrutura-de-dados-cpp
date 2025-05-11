86872f52daac33438f17a702c4af0fbfa0abd4e1
Clecio Freire <144300006+FreireCF@users.noreply.github.com>
2025-05-11 17:37:37 -0300

    inserções em lista encadeada

--- lista.cpp
+++ lista.cpp
@@ -1,57 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-/*Criando uma lista encadeada */
+typedef struct node{ //struct de cada Nó (ou cédula) da nossa lista
+    int elemento; //o elemento do nó
+    struct node *prox; //ponteiro que aponta para o próximo nó na lista
+} Node;
 
-typedef struct Ponto {
-    float x, y;  
-    struct Ponto *prox; //ponteiro que aponta para o próximo item da lista
-} Ponto; //tipo do dado
+void inserirInicio(Node **lista, int novoElemento){ //inserir um número no início da lista
+    Node *novoNo= (Node*) malloc(sizeof(Node)); //criamos e alocamos dinâmicamente um novo nó
 
-/* int main(){
-    Ponto *p0 = (Ponto*) malloc(sizeof(Ponto));
-    p0 ->x = 1;
-    p0->y = 5;
-
-    Ponto *p1 = (Ponto*) malloc(sizeof(Ponto));
-    p1->x = 3;
-    p1->y = 9;
-
-    Ponto *p2 = (Ponto*) malloc(sizeof(Ponto));
-    p2->x = 3;
-    p2->y = 9;
-
-    p0->prox = p1; //o ponteiro prox do p0 aponta para p1, ou seja, o próximo ponto do p0 é o p1
-    p1->prox = p2; 
-    p2->prox = NULL;
-    Dessa forma, teríamos que criar vários pontos e alocações. O que se torna inviável fazer assim. 
-} */
-
-// Para resolver este problema, criaremos uma função com um único ponteiro que recebe o primeiro item, e então, todos os outros apontarão para o próximo
-
-Ponto *primeiroItem=NULL; //ponteiro para apontar o primeiro elemento da lista e ser seguido pelos demais itens. Só é necessário guardar o primeiro e através dele, acessaremos os seguintes.
-
-void adicionarElemento (float x, float y){ //coordenadas de um plano cartesiano
-    Ponto *p = (Ponto*) malloc(sizeof(Ponto));
-    p->x = x;
-    p->y = y;
-    p->prox = primeiroItem; //  p aponta para o primeiro item da lista
-    primeiroItem = p; //primeiroItem recebe o valor de p (primeriro item da lista)
+    if(novoNo){ //conferir se a alocação funcionou 
+        novoNo->elemento = novoElemento; //o elemento do nosso nó, irá receber o int passado com parâmetro
+        novoNo->prox = *lista; //o próximo irá apontar para o topo da lista, ou seja, todos as outras cédulas virão após o novoNo.
+        *lista = novoNo; //adicionando o novo Nó na lista
+    } else {
+        printf("\nErro ao alocar memória!"); 
+    }
 }
 
-void imprimirLista(){
-  Ponto *aux = primeiroItem; //recebe o primeiro item da lista, sem meexer no primeitoItem para que não seja perdido
-    while(aux!=NULL){ //se a lista estiver vazia, não inicializa
-      printf("\n(x: %.2f, y: %.2f)", aux->x, aux->y);
-      aux = aux->prox; //irá receber o seu próximo elemento na lista, até encontrar um NULL e encerrar
+void inserirFinal(Node **lista, int novoElemento){ //inserir um número no fim da lista
+    Node *aux, *novoNo = (Node*) malloc(sizeof(Node));
+
+    if(novoNo){ 
+        novoNo->elemento = novoElemento; 
+        novoNo->prox = NULL; //como iremos inserir no final da lista, o próximo do nosso novo nó tem que ser NULL
+        if(*lista == NULL){ //na primeira inserção, nossa lista estará vazia, então podemos inserir normalmente
+            *lista = novoNo; 
+        } else {
+            aux = *lista; //variável auxiliar para percorrer a lista sem perder a original
+            while(aux->prox!=NULL){ //percorre a lista até o pŕoximo apontar para NULL, ou seja, até o último elemento
+                aux = aux->prox; // auxiliar recebe seu próximo elemento
+            }
+            aux->prox = novoNo; //aṕos encontrarmos o elemento que o seu próximo é null, inserimos nosso novoNo após ele
+        }
+    } else {
+        printf("\nErro ao alocar memória!");
     }
-    printf("\n");
+}
+
+void inserirMeio(Node **lista, int novoElemento, int anterior){ //iremos inserir depois do anterior
+    Node *aux, *novoNo = (Node*) malloc(sizeof(Node));
+
+    if(novoNo){
+        novoNo->elemento = novoElemento;
+        //caso a lista esteja vazia, podemos inserir normalmente
+        if(*lista==NULL){
+            novoNo->prox = NULL;
+            *lista = novoNo;
+        } else {
+            aux = *lista;
+            while(aux->elemento!=anterior && aux->prox!=NULL){ //iremos percorrer até encontrar o anterior informado
+                aux = aux->prox;
+            }
+            novoNo->prox = aux->prox; // novo nó vai apontar para o próximo do nó encontrado
+            aux->prox = novoNo; // o "anterior" agora aponta para o novo nó    
+        }
+    } else {
+        printf("\nErro ao alocar memória!");
+    }
+}
+
+void imprimirLista(Node **lista){
+    Node *aux = *lista;
+    printf("\nLista atual: ");
+    printf("\n[");
+    while(aux!=NULL){
+        printf(" %d ",aux->elemento);
+        aux = aux->prox;
+    }
+    printf("]\n");
+}
+
+int pedirElemento(){
+    int novoElemento;
+    printf("\nDigite um número inteiro para inserir na lista: ");
+        scanf("%d",&novoElemento);
+    return novoElemento;
+}
+
+int menu(){
+    int opc;
+    printf("\n----------------MENU----------------");
+    printf("\n| [0] - Encerrar programa          |");
+    printf("\n| [1] - Inserir no início da lista |");
+    printf("\n| [2] - Inserir no meio da lista   |");
+    printf("\n| [3] - Inserir no final da lista  |");
+    printf("\n| [4] - Ver a lista                |");
+    printf("\n------------------------------------");
+    printf("\nDigite sua opção: ");
+        scanf("%d",&opc);
+    return opc;
 }
 
 int main(){
-    adicionarElemento(1, 5); 
-    adicionarElemento(0, 10);
-    adicionarElemento(3, 9); //adiciona no início da lista
-    imprimirLista();
+    Node *lista = NULL;
+    int opc = -1, elemento = 0;
+    
+    while(opc!=0){
+        opc = menu();
 
+        switch (opc) {
+        case 0:
+            printf("\nEncerrando programa");
+            printf("\n");
+            break;
+        case 1:
+            elemento = pedirElemento();
+           inserirInicio(&lista, elemento);
+            break;
+        case 2:
+            elemento = pedirElemento();
+            imprimirLista(&lista);
+            int ant;
+            printf("\nDeseja inserir depois de qual elemento?");
+                scanf("%d",&ant);
+            inserirMeio(&lista, elemento, ant);
+            break;
+        case 3:
+            elemento = pedirElemento();
+            inserirFinal(&lista, elemento);
+            break;
+        case 4:
+            imprimirLista(&lista);
+            break;
+        default:
+            printf("\nOpção inválida!");
+            break;
+        }
+    }
 }
